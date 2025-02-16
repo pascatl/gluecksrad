5cba45b2c2daa4e5435598015ed3ed1180c6b5a8
pascatl <mail@ptom.de>
2025-02-16 20:49:13 +0100

    change font

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material/styles";
 // MUI-Theme mit Ubuntu als Standard-Schriftart
 const theme = createTheme({
 	typography: {
-		fontFamily: "'Norwester',Roboto, sans-serif",
+		fontFamily: "'Dimbo',Roboto, sans-serif",
 	},
 });
 
